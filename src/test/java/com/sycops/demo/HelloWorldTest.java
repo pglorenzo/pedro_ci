7b5e0fb28d47279d2a2563cc01ffcff6439534dd
pedro gonçalves <goncalves.pedro@epsi.fr>
2020-03-02 17:50:12 +0100

    first commit

---
+++ src/test/java/com/sycops/demo/HelloWorldTest.java
@@ -0,0 +1,19 @@
+package com.sycops.demo;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloWorldTest {
+
+    @Test
+    public void should_return_hello_world() {
+        HelloWorld helloWorld = new HelloWorld();
+
+        String result = helloWorld.helloWorld();
+
+        assertThat(result).isEqualTo("Hello World");
+    }
+
+}
